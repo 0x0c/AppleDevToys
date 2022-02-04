abc778e785cbda7c8083a7cfe3ae81552fca2c75
Akira <akira.matsuda@me.com>
2022-02-04 18:42:05 +0900

    Rename variables

--- DevToys/Module/NumberBase/Interactor/NumberBaseInteractor.swift
+++ DevToys/Module/NumberBase/Interactor/NumberBaseInteractor.swift
@@ -29,20 +29,20 @@ final class NumberBaseInteractor {
     var sections: [CollectionViewSection] = []
     private var cancellable = Set<AnyCancellable>()
 
-    private var inputNumberTypeSelectionViewModel = InputNumberTypeSelectionCellViewModel()
-    private var inputTextFormViewModel = InputNumberTextFormViewModel()
-    private var decimalTextFormViewModel = DecimalTextFormViewModel(isEditable: false)
-    private var hexTextFormViewModel = HexadecimalTextFormViewModel(isEditable: false)
-    private var octalTextFormViewModel = OctalTextFormViewModel(isEditable: false)
-    private var binaryTextFormViewModel = BinaryTextFormViewModel(isEditable: false)
+    private var inputNumberTypeSelection = InputNumberTypeSelectionCellViewModel()
+    private var inputTextForm = InputNumberTextFormViewModel()
+    private var decimalTextForm = DecimalTextFormViewModel(isEditable: false)
+    private var hexTextForm = HexadecimalTextFormViewModel(isEditable: false)
+    private var octalTextForm = OctalTextFormViewModel(isEditable: false)
+    private var binaryTextForm = BinaryTextFormViewModel(isEditable: false)
 
     private var isUpdating = false
 
-    private var formatEnabledViewModel = SwitchCellViewModel(
+    private var formatEnabled = SwitchCellViewModel(
         title: "Format number",
         image: UIImage(systemSymbol: .textformat123)
     )
-    private var uppercaseEnabledViewModel = SwitchCellViewModel(
+    private var uppercaseEnabled = SwitchCellViewModel(
         title: "Uppercase",
         image: UIImage(systemSymbol: .textformat)
     )
@@ -54,84 +54,84 @@ final class NumberBaseInteractor {
     init() {
         sections = [
             ConfigurationSection(items: [
-                .toggle(formatEnabledViewModel),
-                .toggle(uppercaseEnabledViewModel),
-                .inputNumberType(inputNumberTypeSelectionViewModel)
+                .toggle(formatEnabled),
+                .toggle(uppercaseEnabled),
+                .inputNumberType(inputNumberTypeSelection)
             ]),
             NumberInputFormSection.form([
-                inputTextFormViewModel
+                inputTextForm
             ], headerTitle: "Input"),
             NumberInputFormSection.form([
-                decimalTextFormViewModel
+                decimalTextForm
             ], headerTitle: "Decimal"),
             NumberInputFormSection.form([
-                hexTextFormViewModel
+                hexTextForm
             ], headerTitle: "Hex"),
             NumberInputFormSection.form([
-                octalTextFormViewModel
+                octalTextForm
             ], headerTitle: "Octal"),
             NumberInputFormSection.form([
-                binaryTextFormViewModel
+                binaryTextForm
             ], headerTitle: "Binary")
         ]
-        inputNumberTypeSelectionViewModel.$selectedItem.sink { [unowned self] value in
-            self.inputTextFormViewModel.selectedItem = value
-            self.inputTextFormViewModel.update(text: "0")
+        inputNumberTypeSelection.$selectedItem.sink { [unowned self] value in
+            self.inputTextForm.selectedItem = value
+            self.inputTextForm.update(text: "0")
         }.store(in: &cancellable)
-        inputTextFormViewModel.$text.removeDuplicates().sink { [unowned self] text in
-            guard let value = self.inputNumberTypeSelectionViewModel.selectedItem else {
+        inputTextForm.$text.removeDuplicates().sink { [unowned self] text in
+            guard let value = self.inputNumberTypeSelection.selectedItem else {
                 return
             }
             self.update(
                 text,
                 radix: value.radix(),
-                format: self.formatEnabledViewModel.isOn,
-                uppercase: self.uppercaseEnabledViewModel.isOn
+                format: self.formatEnabled.isOn,
+                uppercase: self.uppercaseEnabled.isOn
             )
         }.store(in: &cancellable)
-        decimalTextFormViewModel.$text.removeDuplicates().sink { [unowned self] text in
+        decimalTextForm.$text.removeDuplicates().sink { [unowned self] text in
             self.update(
                 text,
                 radix: 10,
-                format: self.formatEnabledViewModel.isOn,
-                uppercase: self.uppercaseEnabledViewModel.isOn
+                format: self.formatEnabled.isOn,
+                uppercase: self.uppercaseEnabled.isOn
             )
         }.store(in: &cancellable)
-        hexTextFormViewModel.$text.removeDuplicates().sink { [unowned self] text in
+        hexTextForm.$text.removeDuplicates().sink { [unowned self] text in
             self.update(
                 text,
                 radix: 16,
-                format: self.formatEnabledViewModel.isOn,
-                uppercase: self.uppercaseEnabledViewModel.isOn
+                format: self.formatEnabled.isOn,
+                uppercase: self.uppercaseEnabled.isOn
             )
         }.store(in: &cancellable)
-        octalTextFormViewModel.$text.removeDuplicates().sink { [unowned self] text in
+        octalTextForm.$text.removeDuplicates().sink { [unowned self] text in
             self.update(
                 text,
                 radix: 8,
-                format: self.formatEnabledViewModel.isOn,
-                uppercase: self.uppercaseEnabledViewModel.isOn
+                format: self.formatEnabled.isOn,
+                uppercase: self.uppercaseEnabled.isOn
             )
         }.store(in: &cancellable)
-        binaryTextFormViewModel.$text.removeDuplicates().sink { [unowned self] text in
+        binaryTextForm.$text.removeDuplicates().sink { [unowned self] text in
             self.update(
                 text,
                 radix: 2,
-                format: self.formatEnabledViewModel.isOn,
-                uppercase: self.uppercaseEnabledViewModel.isOn
+                format: self.formatEnabled.isOn,
+                uppercase: self.uppercaseEnabled.isOn
             )
         }.store(in: &cancellable)
 
-        formatEnabledViewModel.$isOn.sink { [unowned self] enabled in
-            self.decimalTextFormViewModel.formatText = enabled
-            self.hexTextFormViewModel.formatText = enabled
-            self.octalTextFormViewModel.formatText = enabled
-            self.binaryTextFormViewModel.formatText = enabled
-            self.reload(format: enabled, uppercase: self.uppercaseEnabledViewModel.isOn)
+        formatEnabled.$isOn.sink { [unowned self] enabled in
+            self.decimalTextForm.formatText = enabled
+            self.hexTextForm.formatText = enabled
+            self.octalTextForm.formatText = enabled
+            self.binaryTextForm.formatText = enabled
+            self.reload(format: enabled, uppercase: self.uppercaseEnabled.isOn)
         }.store(in: &cancellable)
-        uppercaseEnabledViewModel.$isOn.sink { [unowned self] enabled in
-            self.hexTextFormViewModel.uppercase = enabled
-            self.reload(format: self.formatEnabledViewModel.isOn, uppercase: enabled)
+        uppercaseEnabled.$isOn.sink { [unowned self] enabled in
+            self.hexTextForm.uppercase = enabled
+            self.reload(format: self.formatEnabled.isOn, uppercase: enabled)
         }.store(in: &cancellable)
     }
 
@@ -149,17 +149,17 @@ final class NumberBaseInteractor {
             isUpdating = false
             return
         }
-        inputTextFormViewModel.update(text: integer.stringfy(radix: radix))
-        decimalTextFormViewModel.update(text: integer.stringfy(radix: 10, format: format))
-        hexTextFormViewModel.update(text: integer.stringfy(radix: 16, format: format))
-        octalTextFormViewModel.update(text: integer.stringfy(radix: 8, format: format))
-        binaryTextFormViewModel.update(text: integer.stringfy(radix: 2, format: format))
+        inputTextForm.update(text: integer.stringfy(radix: radix))
+        decimalTextForm.update(text: integer.stringfy(radix: 10, format: format))
+        hexTextForm.update(text: integer.stringfy(radix: 16, format: format))
+        octalTextForm.update(text: integer.stringfy(radix: 8, format: format))
+        binaryTextForm.update(text: integer.stringfy(radix: 2, format: format))
         isUpdating = false
     }
 
     private func reload(format: Bool, uppercase: Bool) {
         update(
-            decimalTextFormViewModel.text,
+            decimalTextForm.text,
             radix: 10,
             format: format,
             uppercase: uppercase
